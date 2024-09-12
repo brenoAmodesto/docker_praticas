f708503947aa778aa8ed34d5c8ffed089f8d235e
brenoAmodesto <brenoamodesto58@gmail.com>
2024-09-12 16:59:35 -0300

    discord_bot

--- Python/events.py
+++ Python/events.py
@@ -1,10 +1,19 @@
 import docker
+import datetime
+import requests
 
 client = docker.DockerClient(base_url='unix://var/run/docker.sock')
+
+webhook_url = "SUA_URL" ##Webhook discord
 
 for event in client.events(decode=True, filters={"event": "die"}):
     container_id = event["id"]
     container_name = event["Actor"]["Attributes"]["name"]
     epoch_time = event["time"]
+    
+    date = datetime.datetime.fromtimestamp(epoch_time)
 
-    print("O container %s (%s) foi finalizado ás %s" % (container_name, container_id, epoch_time))
+    payload = {"content": "O container %s (%s) foi finalizado ás %s" % (container_name, container_id, date)} ## DICT discord
+
+    print (payload) 
+    requests.post(webhook_url, data=payload)
